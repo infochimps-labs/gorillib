bc1b8a2c78858559ca8faa3b6e6a0d0cd87ef441
Philip (flip) Kromer <flip@infochimps.org>
2011-05-15 06:15:49 -0500

    sporkified, watchrized specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,26 @@
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-require 'rspec'
-require 'gorillib'
+require 'rubygems'
+require 'spork'
 
-# Requires supporting files with custom matchers and macros, etc,
-# in ./support/ and its subdirectories.
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+Spork.prefork do
+  # Loading more in this block will cause your tests to run faster. However,
+  # if you change any configuration or code from libraries loaded here, you'll
+  # need to restart spork for it take effect.
 
-RSpec.configure do |config|
-  
+  $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+  $LOAD_PATH.unshift(File.dirname(__FILE__))
+  require 'rspec'
+  require 'gorillib'
+
+  # Requires supporting files with custom matchers and macros, etc,
+  # in ./support/ and its subdirectories.
+  Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+
+  RSpec.configure do |config|
+
+  end
+end
+
+Spork.each_run do
+  # This code will be run each time you run your specs.
+
 end
