ca4bf7bedd5b805923d22a84328bdb258be67b68
Philip (flip) Kromer <flip@infochimps.org>
2011-05-29 21:21:50 +0100

    organizing hash decorations into modules that we will include with discretion

--- lib/gorillib/hashlike/compact.rb
+++ lib/gorillib/hashlike/compact.rb
@@ -1,3 +1,34 @@
+require 'gorillib/object/blank'
+
+class Hash
+  #
+  # remove all key-value pairs where the value is nil
+  #
+  def compact
+    reject{|key,val| val.nil? }
+  end unless method_defined?(:compact)
+
+  #
+  # Replace the hash with its compacted self
+  #
+  def compact!
+    replace(compact)
+  end unless method_defined?(:compact!)
+
+  #
+  # remove all key-value pairs where the value is blank
+  #
+  def compact_blank
+    reject{|key,val| val.blank? }
+  end unless method_defined?(:compact_blank)
+
+  #
+  # Replace the hash with its compact_blank'ed self
+  #
+  def compact_blank!
+    replace(compact_blank)
+  end unless method_defined?(:compact_blank!)
+end
     #
     # # delete all attributes where the value is blank?, and return self. Contrast with compact!
     # def compact_blank!
