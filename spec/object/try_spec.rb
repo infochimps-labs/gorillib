046e1596492f449ef2d3c58eb516105b91d80a41
Philip (flip) Kromer <flip@infochimps.org>
2011-06-23 22:09:28 -0500

    better try. Yoda will be happy

--- spec/object/try_spec.rb
+++ spec/object/try_spec.rb
@@ -2,8 +2,8 @@ require File.dirname(__FILE__)+'/../spec_helper'
 require 'gorillib/object/try'
 
 class Foo
-  def i_am_a_method_hooray
-    "i was called!"
+  def i_am_a_method_hooray param='hooray'
+    "i was called! #{param}!"
   end
 end
 
@@ -12,8 +12,9 @@ describe Object do
     it 'returns nil if item does not #respond_to? method' do
       Foo.new.try(:i_am_not_a_method).should be_nil
     end
-    it 'calls the method if the item does #respond_to? it' do
-      Foo.new.try(:i_am_a_method_hooray).should == "i was called!"
+    it 'calls the method (with args) if the item does #respond_to? it' do
+      Foo.new.try(:i_am_a_method_hooray).should == "i was called! hooray!"
+      Foo.new.try(:i_am_a_method_hooray, 'yay').should == "i was called! yay!"
     end
   end
 end
