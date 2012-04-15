c4d8956ca0749dbb50bfd051d9caa53318a2bde0
Philip (flip) Kromer <flip@infochimps.org>
2012-04-14 22:49:29 -0500

    nuke_constants test helper. refactored capture_output / dummy_stdio

--- lib/gorillib/test_helpers/capture_output.rb
+++ lib/gorillib/test_helpers/capture_output.rb
@@ -2,27 +2,25 @@ module Gorillib
   module TestHelpers
     module_function
 
+    def dummy_stdio(stdin_text=nil)
+      stdin = stdin_text.nil? ? $stdin : StringIO.new(stdin_text)
+      new_fhs = [stdin,  StringIO.new('', 'w'), StringIO.new('', 'w')]
+      old_fhs = [$stdin, $stdout,               $stderr]
+      begin
+        $stdin, $stdout, $stderr = new_fhs
+        yield
+      ensure
+        $stdin, $stdout, $stderr = old_fhs
+      end
+      new_fhs[1..2]
+    end
+
     #
     # Temporarily sets the global variables $stderr and $stdout to a capturable StringIO;
     # restores them at the end, even if there is an error
     #
     def capture_output
-      local_stdout = StringIO.new('', 'w')
-      local_stderr = StringIO.new('', 'w')
-
-      begin
-        old_stdout = $stdout ; $stdout = local_stdout
-        old_stderr = $stderr ; $stderr = local_stderr
-
-        yield
-
-        $stdout = old_stdout
-        $stderr = old_stderr
-        return [local_stdout, local_stderr]
-      ensure
-        $stdout = old_stdout
-        $stderr = old_stderr
-      end
+      dummy_stdio{ yield }
     end
 
     alias_method :quiet_output, :capture_output
---
+++ lib/gorillib/test_helpers/nuke_constants.rb
@@ -0,0 +1,9 @@
+class Module
+  #
+  # Removes all constants in the module's namespace -- this is useful when
+  # writing specs for metaprogramming methods
+  #
+  def nuke_constants
+    constants.each{|const| remove_const(const) }
+  end
+end
