607e74df4fb46c147a47659c089a62c074b75d35
Philip (flip) Kromer <flip@infochimps.org>
2012-05-02 21:16:16 -0700

    add except/except! to hashlike/slice

--- lib/gorillib/hashlike/slice.rb
+++ lib/gorillib/hashlike/slice.rb
@@ -53,6 +53,29 @@ module Gorillib
       def extract!(*allowed_keys)
         slice!(*self.keys - allowed_keys)
       end unless method_defined?(:extract!)
+
+      # Return a hash that includes everything but the given keys. This is useful for
+      # limiting a set of parameters to everything but a few known toggles:
+      #
+      #   @person.update_attributes(params[:person].except(:admin))
+      #
+      # If the receiver responds to +convert_key+, the method is called on each of the
+      # arguments. This allows +except+ to play nice with hashes with indifferent access
+      # for instance:
+      #
+      #   {:a => 1}.to_mash.except(:a)  # => {}
+      #   {:a => 1}.to_mash.except('a') # => {}
+      #
+      def except(*keys)
+        dup.except!(*keys)
+      end
+
+      # Replaces the hash without the given keys.
+      def except!(*keys)
+        keys.each{|key| delete(key) }
+        self
+      end
+
     end
   end
 end
