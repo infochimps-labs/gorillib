1448d13469a2b9e8f4c76958af9f1b49ec45cee8
Philip (flip) Kromer <flip@infochimps.org>
2012-05-06 14:12:50 -0500

    collections autovivify members more intelligently

--- lib/gorillib/collection.rb
+++ lib/gorillib/collection.rb
@@ -20,10 +20,10 @@ module Gorillib
     delegate :keys, :values, :each_pair, :each_value,     :to => :clxn
     delegate :has_key?, :include?, :length, :size, :empty?, :blank?, :to => :clxn
 
-    def initialize(clxn={}, factory=nil, key_method=DEFAULT_KEY_METHOD)
+    def initialize(factory=nil, key_method=DEFAULT_KEY_METHOD)
       @key_method  = key_method
       @factory     = factory unless factory.nil?
-      @clxn        = convert_collection(clxn)
+      @clxn        = {}
     end
 
     # @return [Array] an array holding the items
@@ -41,8 +41,19 @@ module Gorillib
     alias_method :concat,   :merge!
     alias_method :receive!, :merge!
 
-    def self.receive(clxn)
-      raise NoMethodError.unimplemented_method(self)
+    def self.receive(items, *args)
+      clxn = new(*args)
+      clxn.receive!(items)
+      clxn
+    end
+
+    # Two collections are equal if they have the same class and their contents are equal
+    #
+    # @param [Gorillib::Collection, Object] other The other record to compare
+    # @return [true, false] True if attributes are equal and other is instance of the same Class
+    def ==(other)
+      return false unless other.instance_of?(self.class)
+      clxn == other.send(:clxn)
     end
 
     # Merge the new items into a new collection; given items clobber existing items
@@ -52,10 +63,14 @@ module Gorillib
       dup.merge!(other)
     end
 
-    def create(raw_item)
-      item = factory.receive(raw_item)
+    def create(*args)
+      item = factory.receive(*args)
       self << item
       item
+    end
+
+    def find_or_create(key)
+      fetch(key){ create(key_method => key) }
     end
 
     # Adds an item in-place
@@ -68,7 +83,13 @@ module Gorillib
     # @return [String] string describing the collection's array representation
     def to_s           ; to_a.to_s           ; end
     # @return [String] string describing the collection's array representation
-    def inspect        ; clxn.inspect        ; end
+    def inspect
+      str = "c{ "
+      str << clxn.map do |key, val|
+        "%-20s\t%s" % [key, val.inspect]
+      end.join(",\n   ")
+      str << " }"
+    end
     # @return [Array] serializable array representation of the collection
     def as_json(*args) ; to_a.as_json(*args) ; end
     # @return [String] JSON serialization of the collection's array representation
@@ -76,12 +97,19 @@ module Gorillib
 
   protected
 
+    def convert_value(val)
+      return val unless factory
+      return nil if val.nil?
+      factory.receive(val)
+    end
+
     # - if the given collection responds_to `to_hash`, it is merged into the internal collection; each hash key *must* match the id of its value or results are undefined.
     # - otherwise, it merges a hash generates from the id/value pairs of each object in the given collection.
     def convert_collection(cc)
       return cc.to_hash if cc.respond_to?(:to_hash)
       cc.inject({}) do |acc, val|
-        key = val.public_send(key_method).to_sym
+        val      = convert_value(val)
+        key      = val.public_send(key_method).to_sym
         acc[key] = val
         acc
       end
