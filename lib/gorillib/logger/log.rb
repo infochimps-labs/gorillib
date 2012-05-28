51c9e14bb528c08eefcd670fb5fc2320a11080de
Travis Dempsey <dempsey.travis@gmail.com>
2012-05-28 11:12:14 -0500

    moved log info to gorillib

--- lib/gorillib/logger/log.rb
+++ lib/gorillib/logger/log.rb
@@ -7,6 +7,24 @@ require 'logger'
 #
 ::Log = Logger.new($stderr) unless defined?(::Log)
 
+# unless defined?(Log)
+#   require 'log4r'
+#   Log = Log4r::Logger.new('wukong')
+#   Log.outputters = Log4r::Outputter.stderr
+#   # require 'logger'
+#   # Log = Logger.new(STDERR)
+# end
+
+# require 'log_buddy'; LogBuddy.init :log_to_stdout => false, :logger => Log
+# LogBuddy::Utils.module_eval do
+#   def arg_and_blk_debug(arg, blk)
+#     result = eval(arg, blk.binding)
+#     result_str = obj_to_string(result, :quote_strings => true)
+#     LogBuddy.debug(%[#{arg} = #{result_str}])
+#   end
+# end
+
+
 def Log.dump *args
   self.debug([
       args.map(&:inspect),
