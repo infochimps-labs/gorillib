0c08eb3d518df19658ffd90af8a77c7e77ab716f
Philip (flip) Kromer <flip@infochimps.org>
2012-07-30 04:24:52 -0500

    corrected location of model specs

--- spec/gorillib/model/factories_spec.rb
+++ spec/gorillib/model/factories_spec.rb
@@ -9,12 +9,9 @@ require 'gorillib/string/inflector'
 require 'gorillib/collection'
 require 'gorillib/model/factories'
 
-describe '', :model_spec => true do
-  let(:mock_factory){ Gorillib::Factory::BaseFactory.new }
+require 'factory_test_helpers'
 
-  def test_factory(*args, &block)
-    described_class.new(*args, &block)
-  end
+describe '', :model_spec, :factory_spec do
 
   describe Gorillib::Factory do
     describe 'Factory()' do
@@ -41,61 +38,11 @@ describe '', :model_spec => true do
         Gorillib::Factory(Symbol).should be_a(Gorillib::Factory::SymbolFactory)
         Gorillib::Factory(String).should be_a(Gorillib::Factory::StringFactory)
       end
-      it 'calls Gorillib::Factory.receive' do
+      it 'calls Gorillib::Factory.lookup' do
         x = mock
-        Gorillib::Factory.should_receive(:receive).with(x)
+        Gorillib::Factory.should_receive(:find).with(x)
         Gorillib::Factory(x)
       end
-    end
-  end
-
-  shared_examples_for :it_converts do |conversion_mapping|
-    non_native_ok = conversion_mapping.delete(:non_native_ok)
-    conversion_mapping.each do |obj, expected_result|
-      it "#{obj.inspect} to #{expected_result.inspect}" do
-        subject.native?(  obj).should be_false
-        subject.blankish?(obj).should be_false
-        actual_result = subject.receive(obj)
-        actual_result.should  eql(expected_result)
-        unless non_native_ok then subject.native?(actual_result).should be_true  ; end
-      end
-    end
-  end
-
-  shared_examples_for :it_considers_native do |*native_objs|
-    it native_objs.inspect do
-      native_objs.each do |obj|
-        subject.native?(  obj).should be_true
-        actual_result = subject.receive(obj)
-        actual_result.should equal(obj)
-      end
-    end
-  end
-
-  shared_examples_for :it_considers_blankish do |*blankish_objs|
-    it blankish_objs.inspect do
-      blankish_objs.each do |obj|
-        subject.blankish?(obj).should be_true
-        subject.receive(obj).should be_nil
-      end
-    end
-  end
-
-  shared_examples_for :it_is_a_mismatch_for do |*mismatched_objs|
-    it mismatched_objs.inspect do
-      mismatched_objs.each do |obj|
-        ->{ subject.receive(obj) }.should raise_error(Gorillib::Factory::FactoryMismatchError)
-      end
-    end
-  end
-
-  shared_examples_for :it_is_registered_as do |*keys|
-    it "the factory for #{keys}" do
-      keys.each do |key|
-        Gorillib::Factory(key).should be_a(described_class)
-      end
-      its_factory = Gorillib::Factory(keys.first)
-      Gorillib::Factory.send(:factories).to_hash.select{|key,val| val.equal?(its_factory) }.keys.should == keys
     end
   end
 
@@ -177,7 +124,6 @@ describe '', :model_spec => true do
 
   describe Gorillib::Factory::TimeFactory do
     fuck_wit_dre_day   = Time.new(1993, 2, 18, 8, 8, 0, '-08:00')  # and Everybody's Celebratin'
-    p fuck_wit_dre_day.object_id
     ice_cubes_good_day = Time.utc(1992, 1, 20, 0, 0, 0)
     it_behaves_like :it_considers_native,   Time.now.utc, ice_cubes_good_day
     it_behaves_like :it_converts,           fuck_wit_dre_day => fuck_wit_dre_day.getutc
@@ -195,20 +141,6 @@ describe '', :model_spec => true do
     its(:typename){ should == :time }
   end
 
-  describe Gorillib::Factory::DateFactory do
-    fuck_wit_dre_day   = Date.new(1993, 2, 18)  # and Everybody's Celebratin'
-    ice_cubes_good_day = Date.new(1992, 1, 20)
-    it_behaves_like :it_considers_native,   Date.today, fuck_wit_dre_day, ice_cubes_good_day
-    it_behaves_like :it_converts,           '19930218' => fuck_wit_dre_day, '19920120' => ice_cubes_good_day
-    it_behaves_like :it_converts,           Time.utc(1992, 1, 20, 8, 8, 8) => ice_cubes_good_day
-    before('behaves like it_converts "an unparseable date" to nil'){ subject.stub(:warn) }
-    it_behaves_like :it_converts,           "an unparseable date" => nil, :non_native_ok => true
-    it_behaves_like :it_considers_blankish, nil, ""
-    it_behaves_like :it_is_a_mismatch_for,  :foo, false, []
-    it_behaves_like :it_is_registered_as,   :date, Date
-    its(:typename){ should == :date }
-  end
-
   describe Gorillib::Factory::BooleanFactory do
     it_behaves_like :it_considers_native,   true, false
     it_behaves_like :it_considers_blankish, nil
@@ -216,15 +148,6 @@ describe '', :model_spec => true do
     it_behaves_like :it_converts,           "true" => true,   :true  => true, "0" => true, 0 => true, [] => true, :foo => true, [] => true, Complex(1.5,3) => true, Object.new => true
     it_behaves_like :it_is_registered_as, :boolean
     its(:typename){ should == :boolean }
-  end
-
-  describe Gorillib::Factory::Boolean10Factory do
-    it_behaves_like :it_considers_native,   true, false
-    it_behaves_like :it_considers_blankish, nil
-    it_behaves_like :it_converts,           "false" => false, :false => false, "0" => false, 0 => false
-    it_behaves_like :it_converts,           "true" => true,   :true  => true,  "1" => true,  [] => true, :foo => true, [] => true, Complex(1.5,3) => true, Object.new => true
-    it_behaves_like :it_is_registered_as, :boolean_10
-    its(:typename){ should == :boolean_10 }
   end
 
   describe ::Whatever do
@@ -287,27 +210,6 @@ describe '', :model_spec => true do
 
   # __________________________________________________________________________
 
-  # hand it a collection with entries 1, 2, 3 please
-  shared_examples_for :an_enumerable_factory do
-    it "accepts a factory for its items" do
-      mock_factory.should_receive(:receive).with(1)
-      mock_factory.should_receive(:receive).with(2)
-      mock_factory.should_receive(:receive).with(3)
-      factory = test_factory(:items => mock_factory)
-      factory.receive( collection_123 )
-    end
-    it "can generate an empty collection" do
-      subject.empty_product.should == empty_collection
-    end
-    it "lets you override the empty collection" do
-      ep = mock; ep.should_receive(:try_dup).and_return 'hey'
-      subject = test_factory(:empty_product => ep)
-      subject.empty_product.should == 'hey'
-      subject = test_factory(:empty_product => ->{ 'yo' })
-      subject.empty_product.should == 'yo'
-    end
-  end
-
   describe Gorillib::Factory::HashFactory do
     let(:collection_123){   { 'a' => 1, :b => 2, 'c' => 3 } }
     let(:empty_collection){ {} }
@@ -318,18 +220,19 @@ describe '', :model_spec => true do
     it_behaves_like :an_enumerable_factory
 
     it 'follows examples' do
-      test_factory.receive(collection_123).should == { 'a' => 1, :b => 2, 'c' => 3}
-      test_factory(:keys  => :symbol).receive({'a' => 'x', :b => 'y', 'c' => :z}).should == {:a  => 'x', :b => 'y', :c  => :z}
-      test_factory(:items => :symbol).receive({'a' => 'x', :b => 'y', 'c' => :z}).should == {'a' => :x,  :b => :y,  'c' => :z}
-      autov_factory = test_factory(:empty_product => Hash.new{|h,k| h[k] = {} })
+      described_class.new.receive(collection_123).should == { 'a' => 1, :b => 2, 'c' => 3}
+      described_class.new(:keys  => :symbol).receive({'a' => 'x', :b => 'y', 'c' => :z}).should == {:a  => 'x', :b => 'y', :c  => :z}
+      described_class.new(:items => :symbol).receive({'a' => 'x', :b => 'y', 'c' => :z}).should == {'a' => :x,  :b => :y,  'c' => :z}
+      autov_factory = described_class.new(:empty_product => Hash.new{|h,k| h[k] = {} })
       result = autov_factory.receive({:a => :b})  ; result.should == { :a => :b }
       result[:flanger][:modacity] = 11            ; result.should == { :a => :b,  :flanger => { :modacity => 11 }}
       result2 = autov_factory.receive({:x => :y}) ; result2.should == { :x => :y } ; result.should == { :a => :b,  :flanger => { :modacity => 11 }}
     end
 
     it "accepts a factory for the keys" do
+      mock_factory = mock('factory')
       mock_factory.should_receive(:receive).with(3).and_return("converted!")
-      factory = test_factory(:keys => mock_factory)
+      factory = described_class.new(:keys => mock_factory)
       factory.receive( { 3 => 4 } ).should == { 'converted!' => 4 }
     end
     it_behaves_like :it_is_registered_as, :hash, Hash
@@ -341,9 +244,9 @@ describe '', :model_spec => true do
     let(:empty_collection){ [] }
 
     it 'follows examples' do
-      test_factory.receive([1,2,3]).should == [1,2,3]
-      test_factory(:items         => :symbol).receive(['a', 'b', :c]).should == [:a, :b, :c]
-      test_factory(:empty_product => [1,2,3]).receive([:a,  :b,  :c]).should == [1, 2, 3, :a, :b, :c]
+      described_class.new.receive([1,2,3]).should == [1,2,3]
+      described_class.new(:items         => :symbol).receive(['a', 'b', :c]).should == [:a, :b, :c]
+      described_class.new(:empty_product => [1,2,3]).receive([:a,  :b,  :c]).should == [1, 2, 3, :a, :b, :c]
     end
 
     it_behaves_like :it_considers_blankish, nil
@@ -351,28 +254,6 @@ describe '', :model_spec => true do
     it_behaves_like :an_enumerable_factory
     it_behaves_like :it_is_registered_as, :array, Array
     its(:typename){ should == :array }
-  end
-
-  describe Gorillib::Factory::SetFactory do
-    let(:collection_123){   Set.new([1,2,3]) }
-    let(:empty_collection){ Set.new }
-
-    it 'follows examples' do
-      test_factory.receive([1,2,3]).should == collection_123
-      test_factory(:items  => :symbol).receive(['a', 'b', :c]).should == [:a, :b, :c].to_set
-      test_factory(:empty_product => [1,2,3].to_set).receive([:a,  :b,  :c]).should == [1, 2, 3, :a, :b, :c].to_set
-
-      has_an_empty_array = test_factory.receive( [[]] )
-      has_an_empty_array.should == Set.new( [[]] )
-      has_an_empty_array.first.should == []
-      has_an_empty_array.size.should  == 1
-    end
-
-    it_behaves_like :it_considers_blankish, nil
-    it_behaves_like :it_converts, { [] => Set.new, {} => Set.new, [1,2,3] => [1,2,3].to_set, {:a => :b} => Set.new({:a => :b}), [:a] => [:a].to_set, :non_native_ok => true }
-    it_behaves_like :an_enumerable_factory
-    it_behaves_like :it_is_registered_as, :set, Set
-    its(:typename){ should == :set }
   end
 
 end
