f7143fb2baf8a59945f3a2cf68a409384c187e2f
Dieterich Lawson <dieterich.lawson@gmail.com>
2013-02-17 20:47:18 -0600

    Changed process runner to capture stdin and stdout

--- lib/gorillib/system/runner.rb
+++ lib/gorillib/system/runner.rb
@@ -1,20 +1,36 @@
 #You must include childprocess in your gemfile. 
 #Gorillib (intentionally) does not do so.
 require 'childprocess'
+require 'tempfile'
 
 module Gorillib
   module System
     module Runner
       extend self
 
-      def run *args
-        command = args.delete_at 0
-        opts = args.delete_at -1 if args[-1].is_a? Hash
-        process = ChildProcess.build(command,*args)
-        process.io.inherit!
+      def run(args, options={})
+        options = options.reverse_merge(mirror_io: false)
+        process = ChildProcess.build(*args)
+        out = Tempfile.new('gorillib-runner-out')
+        err = Tempfile.new('gorillib-runner-err')
+        process.io.stdout = out
+        process.io.stderr = err
         process.start
-        process
+        process.wait
+        begin 
+          out.rewind ; err.rewind
+          res = [out.read, err.read, process.exit_code]
+          if options[:mirror_io]
+            $stdout.write res[0]
+            $stderr.write res[1]
+          end
+        ensure
+          out.close ; err.close
+          out.unlink ; err.unlink
+        end
+        res
       end
+
     end
   end
 end
